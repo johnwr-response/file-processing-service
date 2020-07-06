8d47bcbd83389ef569c04a24415262af363f5ffc
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-07-06 15:35:59 +0200

    Init

---
+++ src/main/java/no/responseweb/imagearchive/fileprocessingservice/services/FileProcessingServiceJobProducer.java
@@ -0,0 +1,20 @@
+package no.responseweb.imagearchive.fileprocessingservice.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class FileProcessingServiceJobProducer {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedDelayString = "${response.file.processing.scheduling.fixed-delay-time}", initialDelayString = "${response.file.processing.scheduling.initial-delay-time}")
+    public void pushJobsToWalkers() {
+    }
+
+}
---
+++ src/test/java/no/responseweb/imagearchive/fileprocessingservice/FileProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package no.responseweb.imagearchive.fileprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
